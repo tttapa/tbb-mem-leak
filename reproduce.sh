c1d366c21bc8a3881707c091ba060425e0f854c4
Pieter Pas <pieter.pas@outlook.com>
2019-12-12 01:52:21 +0100

    Add valgrind output

--- reproduce.sh
+++ reproduce.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env sh
 
+set -x
+
 cd $(dirname "$0")/build
-cmake .. -DCMAKE_BUILD_TYPE=Debug
+
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS="-fsanitize=address"
 make -j`nproc`
 ./main
+
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS=""
+make -j`nproc`
+valgrind --leak-check=full ./main
